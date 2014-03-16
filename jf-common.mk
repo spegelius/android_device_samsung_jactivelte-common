1e76c7ebd5c34c3449bd9e6910062212d344e4b4
Spegelius <spegelius@gmail.com>
2014-03-16 16:44:11 +0200

    Camera overlay updates, audio config changes
    Change-Id: I254cafa43783ec70413895d5f7328462fffc52df

--- jf-common.mk
+++ jf-common.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 ## Get non-open-source specific aspects
-$(call inherit-product, vendor/samsung/jf-common/jf-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/jf-common/jf-common-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay
